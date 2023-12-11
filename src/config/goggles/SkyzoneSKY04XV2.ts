10fca47a947239b8400b77cf0e5a52825a968494
Paul Tsang <32644692+paultsanghy@users.noreply.github.com>
2023-12-11 19:47:40 +0000

    Create SkyzoneSKY04XV2.ts

---
+++ src/config/goggles/SkyzoneSKY04XV2.ts
@@ -0,0 +1,17 @@
+import { Config } from "@/src/types/Config";
+
+export const config = {
+    name: "Skyzone SKY04X V2",
+    articles: [
+        "/goggles/skyzone/sky04xv2",
+    ],
+    flags: [
+               {
+            name: "with TBS Fusion",
+            articles: [
+                "/goggles/skyzone/sky04xv2"
+            ]
+        }
+    ],
+    options: []
+} satisfies Config
